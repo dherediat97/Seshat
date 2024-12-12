06294b7fdc302d89c906ead30a9716701fe75872
dherediat <dherediat97@gmail.com>
2024-12-12 18:23:06 +0100

    Update BookItem.tsx

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,18 +1,15 @@
 import {
-  Button,
   Card,
+  CardActionArea,
   CardActions,
   CardContent,
   CardMedia,
-  Grid2,
   IconButton,
   ListItem,
   Typography,
 } from '@mui/material';
 import { Book } from '../types/types';
 import DeleteIcon from '@mui/icons-material/Delete';
-import DetailsIcon from '@mui/icons-material/Book';
-import { useNavigate } from 'react-router-dom';
 
 type BookItemProps = {
   book: Book;
@@ -20,55 +17,40 @@ type BookItemProps = {
 };
 
 export default function BookItem({ book, onActionBook }: BookItemProps) {
-  const navigate = useNavigate();
-
-  const navigateDetailsBook = (book: Book) => {
-    navigate(`/book/${book.isbn}`);
-  };
-
   return (
     <ListItem sx={{ padding: 8, width: 600, marginRight: 8 }}>
       <Card>
-        <CardMedia
-          component={'img'}
-          sx={{
-            width: 300,
-            height: 400,
-            margin: '0 auto',
-            maxHeight: 600,
-            objectFit: 'scale-down',
-          }}
-          image={book.imgSrc}
-          title={book.title}
-        />
-        <CardContent>
-          <Typography component="div" noWrap={true}>
-            {book.title}
-          </Typography>
-          <Typography component="div" variant="caption" noWrap={true}>
-            Autor: {book.authorName}
-          </Typography>
-          <Typography component="div" variant="caption" noWrap={true}>
-            Editorial: {book.publisherName}
-          </Typography>
-        </CardContent>
-        <CardActions>
+        <CardActionArea href={`/book/${book.isbn}`}>
+          <CardMedia
+            component={'img'}
+            sx={{
+              width: 300,
+              height: 400,
+              margin: '0 auto',
+              maxHeight: 600,
+              objectFit: 'cover',
+            }}
+            image={book.imgSrc}
+            title={book.title}
+          />
+
+          <CardContent>
+            <Typography component="div" noWrap={true}>
+              {book.title}
+            </Typography>
+            <Typography component="div" variant="caption" noWrap={true}>
+              Autor: {book.authorName}
+            </Typography>
+            <Typography component="div" variant="caption" noWrap={true}>
+              Editorial: {book.publisherName}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
           {!book.isDeleted ? (
-            <>
-              <IconButton onClick={() => onActionBook(book, false)}>
-                <DeleteIcon />
-              </IconButton>
-            </>
-          ) : (
-            <></>
-          )}
-          {!book.isLocalBook ? (
-            <Grid2 container sx={{ justifyContent: 'flex-end' }}>
-              <Button onClick={() => navigateDetailsBook(book)}>
-                Detalles
-                <DetailsIcon />
-              </Button>
-            </Grid2>
+            <IconButton onClick={() => onActionBook(book, false)}>
+              <DeleteIcon />
+            </IconButton>
           ) : (
             <></>
           )}
